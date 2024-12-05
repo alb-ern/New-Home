0a3f1c25406a230fbb18e7629386632980025336
itu-itis24-akkan23 <akkan23@itu.edu.tr>
2024-12-06 00:45:48 +0300

    numpy introduced and some button progress

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import numpy as np
 from input_ import INPUT
 
 
@@ -9,7 +10,7 @@ class UI:
             center=(UI.center[0], UI.center[1]))
         UI.exit_button_rect = UI.button_disabled.get_rect(
             center=(UI.center[0], UI.center[1]+96))
-        UI.rects = [UI.play_button_rect,UI.exit_button_rect]
+        UI.rects = [UI.play_button_rect, UI.exit_button_rect]
         UI._click_sec = 0
 
     @staticmethod
@@ -22,23 +23,23 @@ class UI:
                 if UI._click_sec:
                     screen.blit(UI.button_mousedown, button_rect.topleft)
                     if UI._click_sec == 1:
-                        if button_rect==UI.play_button_rect:
+                        if button_rect == UI.play_button_rect:
                             INPUT.screen_ui = False
                             INPUT.screen_play = True
-                        elif button_rect==UI.exit_button_rect:
-                            INPUT.screen_play=False
-                            INPUT.screen_ui=False
-                            INPUT.is_game_running=False
+                        elif button_rect == UI.exit_button_rect:
+                            INPUT.screen_play = False
+                            INPUT.screen_ui = False
+                            INPUT.is_game_running = False
                 else:
                     screen.blit(UI.button_mouseover, button_rect.topleft)
             else:
                 screen.blit(UI.button_enabled, button_rect.topleft)
-            if button_rect==UI.play_button_rect:
+            if button_rect == UI.play_button_rect:
                 play_button_text = font.render("START", True, (255, 255, 245))
                 play_button_text_loc = play_button_text.get_rect(
                     center=button_rect.center)
                 screen.blit(play_button_text, play_button_text_loc)
-            elif button_rect==UI.exit_button_rect:
+            elif button_rect == UI.exit_button_rect:
                 exit_button_text = font.render("EXIT", True, (255, 255, 245))
                 exit_button_text_loc = exit_button_text.get_rect(
                     center=button_rect.center)
@@ -60,6 +61,7 @@ class UI:
     def display_info(res_info) -> None:
         UI.center = (res_info.current_w//2, res_info.current_h//2)
 
+
 class BUTTON:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, loc=(0, 0), initial_surface=UI.button_disabled,) -> None:
+        self.rect = initial_surface.get_rect(center=np.add(UI.center, loc))
